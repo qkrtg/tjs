6590756730eba5e37750d76f8f60051e8fda755b
qkrtg <117521013+qkrtg@users.noreply.github.com>
2022-11-26 14:47:42 +0900

    Add files via upload

---
+++ PostActivity.kt
@@ -0,0 +1,26 @@
+package com.example.android_project_sns
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.storage.FirebaseStorage
+
+class PostActivity : AppCompatActivity() {
+    var PICK_IMAGE_FROM_PHOTO = 0
+    var storage : FirebaseStorage? = null
+    var photoUri : Uri? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+
+        storage = FirebaseStorage.getInstance()
+
+        var photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_PHOTO)
+
+
+    }
+}
